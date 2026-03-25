68acdb999787cf581d73bcdbeab3f1803095896e
andyl <andy@r210.com>
2026-03-24 22:14:41 -0700

    Update Editor

--- lib/mix/tasks/progen/action/edit.ex
+++ lib/mix/tasks/progen/action/edit.ex
@@ -43,8 +43,10 @@ defmodule Mix.Tasks.Progen.Action.Edit do
     case ProGen.CLI.source_path(mod) do
       {:ok, path} ->
         if File.exists?(path) do
-          editor = System.get_env("EDITOR") || "vim"
-          System.cmd(editor, [path])
+          [editor | args] = System.get_env("PROGEN_EDITOR") || "vim __FILE__"
+                   |> String.replace("__FILE__", path)
+                   |> String.split()
+          System.cmd(editor, args)
         else
           Mix.raise(
             ~s(Cannot edit "#{name}": source not available.\nOnly path: dependencies can be edited.)
--- lib/mix/tasks/progen/validate/edit.ex
+++ lib/mix/tasks/progen/validate/edit.ex
@@ -42,9 +42,10 @@ defmodule Mix.Tasks.Progen.Validate.Edit do
   defp open_source(name, mod) do
     case ProGen.CLI.source_path(mod) do
       {:ok, path} ->
-        if File.exists?(path) do
-          editor = System.get_env("EDITOR") || "vim"
-          System.cmd(editor, [path])
+          [editor | args] = System.get_env("PROGEN_EDITOR") || "vim __FILE__"
+                   |> String.replace("__FILE__", path)
+                   |> String.split()
+          System.cmd(editor, args)
         else
           Mix.raise(
             ~s(Cannot edit "#{name}": source not available.\nOnly path: dependencies can be edited.)
